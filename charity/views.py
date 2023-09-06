9fce9531b74bb5e626a302eb3329ff75dd5dfcbd
DamianFilimowski <damianFilimowski@gmail.com>
2023-09-06 16:39:08 +0200

    loading institututions on LandingPage

--- charity/views.py
+++ charity/views.py
@@ -11,7 +11,11 @@ from charity.models import *
 class LandingPageView(View):
     def get(self, request):
         bags_quantity = Donation.objects.aggregate(Sum('quantity'))['quantity__sum']
-        return render(request, 'index.html', {'bags_quantity': bags_quantity})
+        foundations = Institution.objects.filter(type=1)
+        ngos = Institution.objects.filter(type=2)
+        locals = Institution.objects.filter(type=3)
+        context = {'bags_quantity': bags_quantity, 'foundations': foundations, 'ngos': ngos, 'locals': locals }
+        return render(request, 'index.html', context)
 
 
 class AddDonationView(View):
